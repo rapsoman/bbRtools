aa5b9b5c6c613e0538fcc7bd8a60c7cae38ab668
votti <vito.zanotelli@gmail.com>
2018-06-06 10:46:13 +0000

    Also adds a test for upper bound censoring.

--- tests/testthat/test_censordat.R
+++ tests/testthat/test_censordat.R
@@ -9,3 +9,11 @@ test_that("Symmetric censoring works correctly",{
   expect_equal(max(out_dat), as.numeric(quantile(dat, 0.995)))
   expect_equal(min(out_dat),as.numeric(quantile(dat, 0.005)))
 })
+
+test_that("Upper bound censoring works correctly",{
+  dat = 1:1000
+  quant = 0.99
+  out_dat = bbRtools::censor_dat(dat, quant=quant, symmetric=F)
+  expect_equal(max(out_dat), as.numeric(quantile(dat, 0.99)))
+  expect_equal(min(out_dat), 1)
+})
